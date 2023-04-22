f1d066dfc9194a546332ad7f5be30a3648f04655
tahir.wani <tahir.waniiqbal@gmail.com>
2023-04-22 22:36:50 +0530

    server response

--- nodeQuickstart/server.js
+++ nodeQuickstart/server.js
@@ -1,9 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    console.log('Hello, World!');
+    console.log(request.url, request.method, request.headers);
+    response.setHeader('content-type', 'text/html');
+    response.write('<html>');
+    response.write('<head><title>server response</title></head>');
+    response.write('<body><h1>');
+    if (request.url == '/home') response.write('welcome home');
+    if (request.url == '/about') response.write('Welcome to About Us page');
+    if (request.url == '/node') response.write('Welcome to my Node Js project');
+    response.write('</h1></body>');
+    response.write('</html>');
+    response.end();
+    //process.exit();
 });
 
-server.listen(3000, () => {
-    console.log('Server listening on port 3000');
+server.listen(4500, () => {
+    console.log('Server listening on port 4500');
 });
