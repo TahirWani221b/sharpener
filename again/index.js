c7aec47068d6bb4dcbaef589e81e80dc1205e10a
tahir.wani <tahir.waniiqbal@gmail.com>
2023-05-29 23:13:12 +0530

    video 10 , get , post request handling -- again

--- again/index.js
+++ again/index.js
@@ -4,16 +4,16 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/form', (req, res, next) => {
+app.get('/form', (req, res, next) => {
     res.send('<form action="/products" method="POST"><input type="text" name="text" ><button type="submit" >Add product</button></form>');
 })
 
-app.use('/products', (req, res, next) => {
+app.post('/products', (req, res, next) => {
     console.log(req.body.text);
     res.redirect('/');
 })
 
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.send('Hello from express');
 })
 app.listen(8080);
