c7c8ed514899b2afd5f258a116fd71bb2ac20583
tahir.wani <tahir.waniiqbal@gmail.com>
2023-06-08 18:48:11 +0530

    skip ejs

--- nodeQuickstart/server2.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const route = require('./route');
-
-http.createServer(route.requestHandler).listen(4500, () => {
-    console.log('Server listening on port 4500');
-});
